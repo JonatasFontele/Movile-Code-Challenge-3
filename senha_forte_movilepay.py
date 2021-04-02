d8f24fd98a584eec4d27d9de021ab387143766da
Jonatas Fontele <jonatasfontele@gmail.com>
2021-04-02 02:00:05 -0300

    Update senha_forte_movilepay.py

--- senha_forte_movilepay.py
+++ senha_forte_movilepay.py
@@ -13,9 +13,6 @@ def testar_password(n, password):
     especial_char_minimo = 1
     valor_minimo = 0
     if 1 <= n <= 100:
-        if n < tamanho_minimo:
-            valor_minimo = tamanho_minimo - n
-
         if len(re.findall("\d+", password)) < numeros_minimo:
             valor_minimo += 1
         if len(re.findall(r"[a-z]", password)) < minusculas_minimo:
@@ -25,20 +22,10 @@ def testar_password(n, password):
         if len(re.findall(r"[!@#$%^&*()+-]", password)) < especial_char_minimo:
             valor_minimo += 1
         if (valor_minimo + len(password)) < n:
-            valor_minimo += (n - len(password))
-            #valor_minimo += (n - valor_minimo)
+            valor_minimo += (n - (valor_minimo + len(password)))
 
-        # if re.match(r"^(?=.*[\d])(?=.*[AZ])(?=.*[az])(?=.*[@#$])[\w\d@#$]{6,12}$", password):
-            # print("match")
-        # else:
-            # print("Not Match")
 
         print(valor_minimo)
 
 
 testar_password(n, password)
-# test_password("J3sus0")
-# test_password("  # Te5t300")
-# test_password("J * 90j12374")
-# test_password("Michheeul")
-# test_password("Monk3y6")
